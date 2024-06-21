f13dd1b281e808aff3135676ab4cf118a99bd931
Dionte Driskell <driskell06@gmail.com>
2024-06-21 13:42:48 -0400

    add names, prices

--- src/compontents/Products.jsx
+++ src/compontents/Products.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './Products.css';
+
+const Products = () => {
+    const products = useSelector((state) => state.products.products);
+    return (
+        <div className="products-container">
+          <h2>{products.length} Product(s) found</h2>
+          <div className="products">
+            {products.map((product) => (
+              <div key={product.id} className="product-card">
+                <img src={product.imageUrl} alt={product.name} />
+                <h3>{product.name}</h3>
+                <p>${product.price}</p>
+                <p>{product.installments}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    };
--- src/compontents/Sizes.jsx
+++ src/compontents/Sizes.jsx
@@ -20,3 +20,5 @@ const Sizes = () => {
         </div>
       );
     };
+
+    export default Sizes;
