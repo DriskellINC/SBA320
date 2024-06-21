83484d3c8277673bace95304582ca7a7e2010b6b
Dionte Driskell <driskell06@gmail.com>
2024-06-21 13:01:32 -0400

    fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
