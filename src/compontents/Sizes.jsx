858e98ece48f1f3db4eec52cca7f1c09f102b4f1
Dionte Driskell <driskell06@gmail.com>
2024-06-21 13:15:45 -0400

    add useSelector

--- src/compontents/Sizes.jsx
+++ src/compontents/Sizes.jsx
@@ -2,3 +2,21 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectSize } from '../features/products/productsSlice';
 import './Sizes.css';
+
+const Sizes = () => {
+    const dispatch = useDispatch();
+    const sizes = useSelector((state) => state.products.sizes);
+
+    return (
+        <div className="sizes-container">
+          <h2>Sizes:</h2>
+          <div className="sizes">
+            {sizes.map((size) => (
+              <button key={size} onClick={() => dispatch(selectSize(size))}>
+                {size}
+              </button>
+            ))}
+          </div>
+        </div>
+      );
+    };
